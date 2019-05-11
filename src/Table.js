780dfecbe51ab5ff0c6494cf468652e4478524da
AKaarna <akaarna@unl.test>
2019-05-11 14:57:37 +0300

    props

--- src/Table.js
+++ src/Table.js
@@ -31,7 +31,8 @@ const TableHeader = () => {
 
 const TableBody = props => {
   const rows = props.characterData.map((row, index) => {
-    console.log(row.name)
+    console.log(index, row.name, row.job)
+    // key={index} must be used for unique identification.
     return (
       <tr key={index}>
         <td>{row.name}</td>
