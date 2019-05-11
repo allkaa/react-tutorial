cc4d38673f0e1b8240e97c5c746a9fd598ebcb90
AKaarna <akaarna@unl.test>
2019-05-11 10:50:37 +0300

    app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello, React from App!</h1>
+        <h1>Hello, React from App.js file with export default App!</h1>
       </div>
     )
   }
