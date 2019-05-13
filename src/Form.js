3e05af83bf750491df46c208d34a1fcc54486ff7
AKaarna <akaarna@unl.test>
2019-05-13 10:26:51 +0300

    state and props asyn

--- src/Form.js
+++ src/Form.js
@@ -13,38 +13,42 @@ class Form extends Component {
       job: '',
     }
 
+    console.log('Form constructor this.initialState')
+    console.log(this.initialState)
     console.log('Form constructor this.state before setting from this.initialState')
     console.log(this.state)
-    this.state = this.initialState // set Form this.state.
+    this.state = this.initialState // set Form this.state directly without re-rendering.
     console.log('Form constructor this.state after setting from this.initialState')
     console.log(this.state)
   }
 
   handleChange = event => {
     const { name, value } = event.target
-  
+    console.log('Form handeChange event event.target')
+    console.log(event.target)
+    
     console.log('Form handeChange event this.state before setting event {name: value}')
     console.log(this.state)
-    this.setState({ // set Form this.state and trigger re-rendering with childs.
+    this.setState({ // set Form this.state asyn and trigger re-rendering with childs.
       [name]: value,
     })
-    console.log('Form handeChange event this.state after setting event {name: value}')
+    console.log('Form handeChange event this.state after asyn setting event {name: value}')
     console.log(this.state)
-    console.log('Form handeChange event this.initialState')
-    console.log(this.initialState)
+    //console.log('Form handeChange event this.initialState')
+    //console.log(this.initialState)
   }
 
   submitForm = () => {
-    console.log('Form submitForm event this.state before hanleSubmit')
+    console.log('Form submitForm event this.state before calling handleSubmit')
     console.log(this.state)
     this.props.handleSubmit(this.state) // call App handleFormSubmit() with Form this.state.
-    console.log('Form submitForm event this.state after hanleSubmit')
+    console.log('Form submitForm event this.state after calling handleSubmit')
     console.log(this.state)
-    this.setState(this.initialState) // set Form this.state to initial and trigger re-rendering with childs.
-    console.log('Form submitForm event this.state after reset to this.initalState')
+    this.setState(this.initialState) // asyn set Form this.state to initial and trigger re-rendering with childs.
+    console.log('Form submitForm event this.state after asyn reset to this.initalState')
     console.log(this.state)
-    console.log('Form submitForm event this.initialState')
-    console.log(this.initialState)
+    //console.log('Form submitForm event this.initialState')
+    //console.log(this.initialState)
   }
   // A class component must include render(), and the return statement can only return ONE parent element:
   render() {
