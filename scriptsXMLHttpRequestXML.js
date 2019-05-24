9ca19c7912753e2291a4c8f90137d04fe47c3dbf
AKaarna <akaarna@unl.test>
2019-05-24 09:03:55 +0300

    Xhr

---
+++ scriptsXMLHttpRequestXML.js
@@ -0,0 +1,134 @@
+const app = document.getElementById('root')
+
+const logo = document.createElement('img')
+logo.src = 'logo.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
+
+//return; // NB! return can not be used in browser DOM.
+
+//*
+var xhr = new XMLHttpRequest;
+xhr.open('GET', 'http://10.8.194.3:42001/?testDebian', true);
+
+// If specified, responseType must be empty string or "document"
+xhr.responseType = 'document';
+
+// Force the response to be parsed as XML
+xhr.overrideMimeType('text/xml');
+
+xhr.onload = function () {
+  let docXml
+  const p = document.createElement('p')
+  if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+    //console.log(xhr.response);
+    docXml = xhr.responseXML
+    //console.log(docXml);
+    let xmlS = new XMLSerializer();
+    let xmlString = xmlS.serializeToString(docXml);
+    p.textContent = xmlString
+  }
+  else {
+    p.textContent = `Request onload error status ${xhr.status}`
+  }
+  // Append the container element with p.
+  container.appendChild(p)
+}
+
+xhr.onerror = () => {
+  const p = document.createElement('p')
+  p.textContent = `Request failed -> onerror event occured.`
+  container.appendChild(p)
+}
+
+xhr.ontimeout = () => {
+  const p = document.createElement('p')
+  p.textContent = `Request failed -> ontimeout event occured`
+  container.appendChild(p)
+}
+
+xhr.send();
+//*/
+
+/*
+var request = new XMLHttpRequest()
+console.log('===============> request.readyState: ' + request.readyState)
+
+request.onreadystatechange = () => {
+  console.log('===============> request.readyState: ' + request.readyState)
+}
+console.log('===============> request.open begin')
+//request.open('GET', 'https://ghibliapi.herokuapp.com/films', true) // true is for asyn open mode, false for syn but depricated.
+request.open('GET', 'http://10.8.194.3:42000/?testDebian', true) // true is for asyn open mode, false for syn but depricated.
+console.log('===============> request.open done')
+console.log('===============> request.send() begin')
+request.send() // if syn open case, takes long time up to onreadystatechange = 4.
+console.log('===============> request.send() done')
+//*/
+
+/* Syn open/send case (depricated):
+console.log('===============> Syn Begin of this.responseText <=================')
+//console.log(request.responseText)
+console.log('===============> Syn End of this.responseText <=================')
+// Begin accessing JSON data here
+var dataSyn = JSON.parse(request.response)
+
+console.log('=======================> Syn request.status: ' + request.status)
+if (request.status >= 200 && request.status < 400) {
+  dataSyn.forEach(movie => {
+    console.log(movie.title)
+  })
+} else {
+  console.log('=======================> Syn request.status: ' + request.status)
+}
+console.log('===============> Syn request finished')
+*/
+
+/*
+// NB! request.onload works only with asyn requsest open mode.
+request.onload = function() {
+  console.log('===============> request.onload event activated')
+  console.log('===============> Begin of this.responseText <=================')
+  //console.log(this.responseText)
+  console.log('===============> End of this.responseText <=================')
+  // Begin accessing JSON data here
+  //var data = JSON.parse(this.response)
+  var data = request.XMLHttpRequest
+
+  console.log('=======================> request.onload event this.status: ' + this.status)
+  if (this.status >= 200 && this.status < 400) {
+    data.forEach(movie => {
+      // Create a div with a card class
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      // Create an h1 and set the text content to the film's title
+      const h1 = document.createElement('h1')
+      h1.textContent = movie.title
+
+      // Create a p and set the text content to the film's description
+      const p = document.createElement('p')
+      movie.description = movie.description.substring(0, 300) // Limit to 300 chars
+      p.textContent = `${movie.description}...` // End with an ellipses
+
+      // Append the cards to the container element
+      container.appendChild(card)
+
+      // Each card will contain an h1 and a p
+      card.appendChild(h1)
+      card.appendChild(p)
+    })
+  } else {
+    console.log('=======================> request.onload event error in this.status: ' + this.status)
+    const errorMessage = document.createElement('marquee')
+    //let errMsg = "Gah, it's not working!"
+    errorMessage.textContent = "Gah, it's not working!"
+    app.appendChild(errorMessage)
+  }
+  console.log('===============> request.onload event finished')
+}
+*/
---
+++ src/ApiXhr.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  state = {
+    data: [],
+  }
+
+  // Code is invoked after the component is mounted/inserted into the DOM tree.
+  componentDidMount() {
+    const url =
+      'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
+
+    fetch(url)
+      .then(result => {
+        // result is fetch(url) Response object
+        console.log(result)
+        return result.json()
+      })
+      .then(result => {
+        // result is JSON array.
+        console.log(result)
+        this.setState({
+          data: result,
+        })
+      })
+  }
+
+  render() {
+    const { data } = this.state
+
+    const result = data.map((entry, index) => {
+      return <li key={index}>{entry}</li>
+    })
+
+    return <ul>{result}</ul>
+  }
+}
+
+export default App
