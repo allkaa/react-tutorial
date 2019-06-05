fc0195528cf9cb278f7509a1b21e67f749e81ea4
AKaarna <akaarna@unl.test>
2019-06-05 10:38:06 +0300

    college

--- src/AppCollege.js
+++ src/AppCollege.js
@@ -1,30 +1,8 @@
 // Traditional very first custom class component - App
 // (capitalize custom components to differentiate them from regular HTML elements).
 import React, { Component } from 'react'
-import Table from './Table'
-import Form from './Form'
 
-  /*
-  state = { characters: [
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'MacDuck2',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
-  ]}
-  */
- class App extends Component {
+class App extends Component {
   // No constructor(props) initially class declaration used
   /*
   state = {characters: [
@@ -53,31 +31,6 @@ import Form from './Form'
     console.log(this.state)
   }
 
-  removeRow = index => {
-    const { characters } = this.state
-  
-    console.log('App removeRow this.state before')
-    console.log(this.state)
-    this.setState({ // set App this.state and trigger re-rendering with childs.
-      characters: characters.filter((row_object, i) => {
-        return i !== index
-      }),
-    })
-    console.log('App removeRow this.state after asyn row delete')
-    console.log(this.state)
-  }  
-
-  // Below character actuallly is objet {name: value}
-  handleFormSubmit = character => {
-    console.log('App handleFormSubmit event object character before asyn setState')
-    console.log(character)
-    console.log('App handleFormSubmit event this.state before asyn setState')
-    console.log(this.state)
-     // asyn set App this.state and trigger re-rendering with childs.
-    this.setState({ characters: [...this.state.characters, character] })
-    console.log('App handleFormSubmit event this.state after asyn setState')
-    console.log(this.state)
-  }
   // A class component must include render(), and the return statement can only return ONE parent element:
   render() {
     //const { characters } = this.state
@@ -87,8 +40,7 @@ import Form from './Form'
       <div className="container">
         {/*<Table characterData={this.state.characters} />*/}
         {/*<Table characterData={this.state.characters} removeRow={this.removeRow} />*/}
-        <Table characterData={this.state.characters} removeRow={this.removeRow} />
-        <Form handleSubmit={this.handleFormSubmit} />
+        <h1>Hello, React from AppCollege.js file with export default App!</h1>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 //import './styleCards.css'
+import './styleCollege.css'
+
 //import App from './AppInitial'
 //import App from './App'
 //import App from './Api'
