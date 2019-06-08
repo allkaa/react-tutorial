07586cf6eed05784d2a19f1000cc2728cb6705de
AKaarna <akaarna@unl.test>
2019-06-08 10:00:40 +0300

    max-width

--- src/AppBoxesAdv.js
+++ src/AppBoxesAdv.js
@@ -25,29 +25,18 @@ class App extends Component {
   // Code is invoked after the component is mounted/inserted into the DOM tree.
   componentDidMount() {
     let one = document.querySelector('.one');
-    let two = document.querySelector('.two');
     let p1 = document.querySelector('#p1');
-    let p2 = document.querySelector('#p2');
 
     let txt
     let width = one.offsetWidth;
     let height = one.offsetHeight;
-    txt = 'Boxes default box-sizing Width: ' + width + 'px, Height: ' + height + 'px. ';
+    txt = 'Box with box-sizing:border-box Width: ' + width + 'px, Height: ' + height + 'px. ';
     width = p1.offsetWidth;
     height = p1.offsetHeight;
-    txt = txt + '<p1> Width: ' + width + 'px, Height: ' + height + 'px. ';
+    txt = txt + 'For <p1> Width: ' + width + 'px, Height: ' + height + 'px. ';
     let p1i = document.querySelector('#p1info');
     p1i.textContent = txt
 
-    width = two.offsetWidth;
-    height = two.offsetHeight;
-    //two.textContent = 'Boxes box-sizing: border-box Width two: ' + width + 'px, Height: ' + height + 'px.';
-    txt = 'Boxes box-sizing: border-box Width: ' + width + 'px, Height: ' + height + 'px. ';
-    width = p2.offsetWidth;
-    height = p2.offsetHeight;
-    txt = txt + '<p2> Width: ' + width + 'px, Height: ' + height + 'px. ';
-    let p2i = document.querySelector('#p2info');
-    p2i.textContent = txt
   }
 
   // A class component must include render(), and the return statement can only return ONE parent element:
@@ -60,13 +49,9 @@ class App extends Component {
         {/*<Table characterData={this.state.characters} />*/}
         <h1>Box heights don't observe percentage lengths; box height always adopts the height of the box content, unless a specific absolute height is set (e.g. pixel-s or (r)em-s.)  </h1>
         <div class="one">
-          <p id='p1'>Boxes sample - default box-sizing:content-box</p>
+          <p id='p1'>Boxes sample - box-sizing:border-box</p>
         </div>
         <p id='p1info'>piInfo</p>
-        <div class="two">
-          <p id='p2'>Boxes sample -  box-sizing: border-box;</p>
-        </div>        
-        <p id='p2info'>piInfo</p>
       </div>
     )}
 }
