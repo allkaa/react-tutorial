11120668576a8b1aee90c791dddb8506e6518581
AKaarna <akaarna@unl.test>
2019-06-11 14:55:37 +0300

    hooks final

--- src/AppHooks.js
+++ src/AppHooks.js
@@ -4,73 +4,43 @@ import React, { Component } from 'react'
 import {useState} from 'react'; // import Hooks.
 
 function Dog(props){
-
-  // declaring a new state variable. Has 2 constants. 
-  // One to name and the other to make changes to the state.
-  console.log('========> Dog object dog <==========')
-  //console.log(props)
-  //console.log(props.dogInfoArr[0].name)
-  //console.log(props.dogInfoArr[0].mode)
-  let newName;
-  //const [dog, setDog] = useState(props.dogInfoArr[0].name);
+  // Declaring a new Hooks state with 2 constants. 
+  // One to dog info object and the other function to make changes to the state.
   const [dog, setDog] = useState(props.dogInfoArr[0]); // use Hooks useState() function and set const dog and setDog.
-  console.log(dog);
-  //console.log(setDog.toString()); // // show function setDog source text or native code e.g. function () { [native code] }
-  //console.log(setDog.toString); // show function toString native code e.g. f toString() { [native code] }
+  let newInfo;
   if (dog.name  === props.dogInfoArr[0].name) {
-    newName = {name: 'Bonny', mode: 'good'};
+    newInfo = {name: 'Bonny', mode: 'good'};
   }
   else if (dog.name === 'Bonny') {
-    newName = {name: 'Claid', mode: 'bark'};
+    newInfo = {name: 'Claid', mode: 'bark'};
   }
   else if (dog.name === 'Claid') {
-    newName = props.dogInfoArr[0];
+    newInfo = props.dogInfoArr[0];
   }
 
   // use imported Hooks setDog function.
   return ( 
     <div> 
       <p>{dog.name} goes {dog.mode}!</p>
-      <button onClick={() => setDog(newName)}>Press me to change dog info</button>
+      <button onClick={() => setDog(newInfo)}>Press me to change dog info</button>
     </div>
   )
-} // End of Dog.
+}
 
 class App extends Component {
-  // No constructor(props) initially - class declaration used:
-  /*
-  state = {infoArr: [
-    {
-      name: 'Max',
-      mode: 'woof',
-    },
-  ]}
-  */
-
-  // We'll need the constructor() to use `this`, and to receive the props of the parent.
   constructor(props) {
     super(props)
-    console.log('App constructor props')
-    console.log(props)
-
-    console.log('App constructor this.state before')
-    console.log(this.state)
+    // Initial info array about dog.
     this.state = {infoArr: [
       {
         name: 'Max',
         mode: 'woof',
       },
     ]}
-    console.log('App constructor this.state after')
-    console.log(this.state)
   }
 
   // A class component must include render(), and the return statement can only return ONE parent element:
   render() {
-    //const { characters } = this.state
-    // Render of re-render with Table and Form childs.
-    //console.log(Dog.toString()); // show function Dog source text (or native code)
-    console.log('========> App render <==========')
     return (
       <div className="container">
         <Dog dogInfoArr={this.state.infoArr}/>
