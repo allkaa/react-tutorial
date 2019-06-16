15b93e5353032e29c8dcedb9827f2deac0df7e3c
AKaarna <akaarna@unl.test>
2019-06-16 16:24:58 +0300

    flex

---
+++ src/AppLayoutFlex2-wrap.js
@@ -0,0 +1,150 @@
+// Traditional very first custom class component - App
+// (capitalize custom components to differentiate them from regular HTML elements).
+import React, { Component } from 'react'
+//import logo from './logo.svg'; // logo.svg in srs dir
+
+class App extends Component {
+
+  // Declare state as Public field and it will be seen in constructor immediately (always present).
+  // By declaring fields up-front with or without a default value,
+  // class definitions become more self-documenting, and the fields are always present.
+  // NB! Public and Private fields declaration is experimental and it works thru Babel compilation or on some browsers.
+  /*
+  state = {
+    name: 'Dee',
+    job: 'Aspring actress',
+  }
+  */
+
+  // We'll need the constructor() to use `this`, and to receive the props of the parent.
+  constructor(props) {
+    console.log('App constructor initial props before super(props)')
+    console.log(props)
+    super(props) // call the super class constructor and pass in the props parameter
+    console.log('App constructor initial props after super(props)')
+    console.log(props)
+
+    console.log('App constructor this.state before settings test data')
+    console.log(this.state)
+    // Declare state in constructor /*
+    this.state = {
+        name: 'Dee',
+        job: 'Aspring actress',
+      }
+    //*/
+    console.log('App constructor this.state after settings test data')
+    console.log(this.state)
+  }
+
+  // Code is invoked after the component is mounted/inserted into the DOM tree.
+  componentDidMount() {
+    console.log('========> App componentDidMount <==========')
+  }
+
+  handleOnClick = event => {
+    console.log('App handeChange event event.target')
+    console.log(event.target)
+    
+    let nameToggle
+    if (this.state.name === 'Alex') {
+      nameToggle = 'Alex2'
+    }
+    else {
+      nameToggle = 'Alex'
+    }
+    console.log('App handleOnClick event this.state before setting event {name: value}')
+    console.log(this.state)
+    this.setState({ // set App this.state asyn and trigger re-rendering with childs.
+      name: nameToggle, job: 'SW Engineer'
+    })
+    console.log('App handleOnClick event this.state after asyn setting event {name: value}')
+    console.log(this.state)
+    //console.log('App handleOnClick event this.initialState')
+    //console.log(this.initialState)
+  }
+
+  // A class component must include render(), and the return statement can only return ONE parent element:
+  render() {
+    //const { characters } = this.state
+    // Render of re-render with Table and Form childs.
+    console.log('========> App render <==========')
+    // {/*<img src={logo} alt="React Logo" className = 'logo' ></img>*/}
+    // {/*<img src={logo} alt="React Logo" className = 'logo' ></img>*/}
+    // {/* <h1>Positioning</h1> */}
+
+    return (
+      <div className="container">
+
+        <header>
+          <h1>Sample flexbox example</h1>
+        </header>
+
+        <section>
+          <article>
+            <h2>First article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+          </article>
+
+          <article>
+            <h2>Second article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+          </article>
+
+          <article>
+            <h2>Third article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+            <p>Cray food truck brunch, XOXO +1 keffiyeh pickled chambray waistcoat ennui. Organic small batch paleo 8-bit. Intelligentsia umami wayfarers pickled, asymmetrical kombucha letterpress kitsch leggings cold-pressed squid chartreuse put a bird on it. Listicle pickled man bun cornhole heirloom art party.</p>
+          </article>
+          <article>
+            <h2>First article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+          </article>
+
+          <article>
+            <h2>Second article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+          </article>
+
+          <article>
+            <h2>Third article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+            <p>Cray food truck brunch, XOXO +1 keffiyeh pickled chambray waistcoat ennui. Organic small batch paleo 8-bit. Intelligentsia umami wayfarers pickled, asymmetrical kombucha letterpress kitsch leggings cold-pressed squid chartreuse put a bird on it. Listicle pickled man bun cornhole heirloom art party.</p>
+          </article>
+          <article>
+            <h2>First article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+          </article>
+
+          <article>
+            <h2>Second article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+          </article>
+
+          <article>
+            <h2>Third article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+            <p>Cray food truck brunch, XOXO +1 keffiyeh pickled chambray waistcoat ennui. Organic small batch paleo 8-bit. Intelligentsia umami wayfarers pickled, asymmetrical kombucha letterpress kitsch leggings cold-pressed squid chartreuse put a bird on it. Listicle pickled man bun cornhole heirloom art party.</p>
+          </article>
+          <article>
+            <h2>First article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+          </article>
+
+          <article>
+            <h2>Second article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+          </article>
+
+          <article>
+            <h2>Third article</h2>
+            <p>Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.</p>
+            <p>Cray food truck brunch, XOXO +1 keffiyeh pickled chambray waistcoat ennui. Organic small batch paleo 8-bit. Intelligentsia umami wayfarers pickled, asymmetrical kombucha letterpress kitsch leggings cold-pressed squid chartreuse put a bird on it. Listicle pickled man bun cornhole heirloom art party.</p>
+          </article>
+        </section>
+
+      </div>
+    )
+  }
+}
+
+export default App
