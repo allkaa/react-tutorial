4ba2ab23e639c68085ba86ed99f8ceb99d34db9a
AKaarna <akaarna@unl.test>
2019-06-18 12:21:55 +0300

    line-based placement

--- ttt.js
+++ ttt.js
@@ -52,12 +52,13 @@ console.log(square);
 
 /* BasedPlacement
 Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny pack portland everyday carry vinyl typewriter. Tacos PBR&B pork belly, everyday carry ennui pickled sriracha normcore hashtag polaroid single-origin coffee cold-pressed. PBR&B tattooed trust fund twee, leggings salvia iPhone photo booth health goth gastropub hammock.
+
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="utf-8">
-    <title>CSS Grid starting point</title>
+    <title>CSS Grid - line-based placement starting point</title>
     <style>
         body {
             width: 90%;
@@ -66,26 +67,39 @@ Tacos actually microdosing, pour-over semiotics banjo chicharrones retro fanny p
             font: .9em/1.2 Arial, Helvetica, sans-serif;
         }
 
-        .container > div {
+        .container {
+            display: grid;
+            grid-template-columns: 1fr 3fr;
+            grid-gap: 20px;
+        }
+
+        header,
+        footer {
             border-radius: 5px;
             padding: 10px;
             background-color: rgb(207,232,220);
             border: 2px solid rgb(79,185,227);
         }
+
+        aside {
+            border-right: 1px solid #999;
+        }
     </style>
 </head>
 
 <body>
-    <h1>Simple grid example</h1>
 
     <div class="container">
-        <div>One</div>
-        <div>Two</div>
-        <div>Three</div>
-        <div>Four</div>
-        <div>Five</div>
-        <div>Six</div>
-        <div>Seven</div>
+        <header>This is my lovely blog</header>
+        <article>
+                <h1>My article</h1>
+                <p>Duis felis orci, pulvinar id metus ut, rutrum luctus orci. Cras porttitor imperdiet nunc, at ultricies tellus laoreet sit amet. Sed auctor cursus massa at porta. Integer ligula ipsum, tristique sit amet orci vel, viverra egestas ligula. Curabitur vehicula tellus neque, ac ornare ex malesuada et. In vitae convallis lacus. Aliquam erat volutpat. Suspendisse ac imperdiet turpis. Aenean finibus sollicitudin eros pharetra congue. Duis ornare egestas augue ut luctus. Proin blandit quam nec lacus varius commodo et a urna. Ut id ornare felis, eget fermentum sapien.</p>
+
+                <p>Nam vulputate diam nec tempor bibendum. Donec luctus augue eget malesuada ultrices. Phasellus turpis est, posuere sit amet dapibus ut, facilisis sed est. Nam id risus quis ante semper consectetur eget aliquam lorem. Vivamus tristique elit dolor, sed pretium metus suscipit vel. Mauris ultricies lectus sed lobortis finibus. Vivamus eu urna eget velit cursus viverra quis vestibulum sem. Aliquam tincidunt eget purus in interdum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+        </article>
+        <aside><h2>Other things</h2>
+            <p>Nam vulputate diam nec tempor bibendum. Donec luctus augue eget malesuada ultrices. Phasellus turpis est, posuere sit amet dapibus ut, facilisis sed est.</p></aside>
+        <footer>Contact me@mysite.com</footer>
     </div>
 
 </body>
