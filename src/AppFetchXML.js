b8d8ca88a939440985b3942a40ec98a49cb4e7ca
AKaarna <akaarna@unl.test>
2019-08-23 13:08:18 +0300

    Fetch and XHR POST and corse

--- src/AppFetchXML.js
+++ src/AppFetchXML.js
@@ -7,25 +7,67 @@ class App extends Component {
 
   // Code is invoked after the component is mounted/inserted into the DOM tree.
   componentDidMount() {
-    const url =
-      'http://10.8.194.3:42001/?testDebian'
+    const url = 'http://10.8.194.3:42001/?testDebian';
+    let data = {username: 'example'};
+    console.log(JSON.stringify(data)); // will be sting '{"username":"example"}'
 
+    // body: '{"param1": "test"}', // data can be `string` or {object}!
+    // If 'Content-Type': 'text/plain' in POST request then no CORS error!
+    // If 'Content-Type': 'application/json' in POST request then Browser will reports CORS error:
+    /*
+    Access to fetch at 'http://10.8.194.3:42001/?testDebian' from origin 'http://unl.test:8080' has been blocked
+    by CORS policy: Request header field content-type is not allowed by Access-Control-Allow-Headers in preflight response.
+    Cross-Origin Read Blocking (CORB) blocked cross-origin response http://10.8.194.3:42001/?testDebian 
+    with MIME type text/xml.
+    See https://www.chromestatus.com/feature/5629709824032768 for more details.
+    NB! If mode: 'no-cors' used then error wll be:
+    Cross-Origin Read Blocking (CORB) blocked cross-origin response http://10.8.194.3:42001/?testDebian 
+    with MIME type text/xml. See https://www.chromestatus.com/feature/5629709824032768 for more details.
+    Also
+    Cross-Origin Read Blocking (CORB) blocked cross-origin response http://10.8.194.3:42001/?testDebian with MIME type
+     application/json. See https://www.chromestatus.com/feature/5629709824032768 for more details.
+
+    mode: 'no-cors', // no-cors, cors, *same-origin
+    */
+    fetch(url, {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify(data), // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'text/plain'
+      }
+    })
+    .then(result => {
+      // result is fetch(url) Response object
+      console.log(result)
+      return result.text() // return result.json()
+    })
+    .then(result => {
+      // result is JSON array.
+      console.log(result)
+      this.setState({
+        data: result,
+      })
+    })
+  
+    /*
     fetch(url)
-      .then(result => {
-        // result is fetch(url) Response object
-        console.log(result)
-        return result.text() // return result.json()
+    .then(result => {
+      // result is fetch(url) Response object
+      console.log(result)
+      return result.text() // return result.json()
+    })
+    .then(result => {
+      // result is JSON array.
+      console.log(result)
+      this.setState({
+        data: result,
       })
-      .then(result => {
-        // result is JSON array.
-        console.log(result)
-        this.setState({
-          data: result,
-        })
-      })
+    })
+    */
+  
   }
 
-    /*
+/*
     render () {
     const result = data.map((entry, index) => {
       return <li key={index}>{entry}</li>
--- src/AppXhrXml.js
+++ src/AppXhrXml.js
@@ -10,12 +10,17 @@ class App extends Component {
     let txtErr 
     //let objThis = this
     const xhr = new XMLHttpRequest();
+    /*
     xhr.open('GET', 'http://10.8.194.3:42001/?testDebian', true);
+    */
+    xhr.open('POST', 'http://10.8.194.3:42001/?testDebian', true);
     // If specified, responseType must be empty string or "document"
     xhr.responseType = 'document';
     // Force the response to be parsed as XML
     xhr.overrideMimeType('text/xml');
     
+
+    ///* GET or PUT state case using onload event.
     xhr.onload = () => {
       console.log(xhr.getAllResponseHeaders());
       let docXml
@@ -34,12 +39,41 @@ class App extends Component {
         })
       }
       else {
-        txtErr = `Request onload error - status ${xhr.status}`
+        txtErr = `Request onload error - status ${xhr.status}, readyState ${xhr.readyState}`
         console.log(txtErr)
         this.setState({ data: txtErr,})
         }
     }
-    
+    //*/
+
+    /*
+    // GET or PUT case using onreadystatechange event;
+    xhr.onreadystatechange = () => { // Call a function when the state changes.
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        // Request finished. Do processing here.
+        console.log(xhr.getAllResponseHeaders());
+        let docXml
+        //console.log(xhr.response);
+        docXml = xhr.responseXML
+        //console.log(docXml);
+        let xmlS = new XMLSerializer();
+        let xmlString = xmlS.serializeToString(docXml);
+        console.log(xmlString)
+        let nodeValue = docXml.getElementsByTagName("result")[0].childNodes[0].nodeValue; // get <result> tag text value.
+        let nodeValue2 = docXml.getElementsByTagName("comment")[0].childNodes[0].nodeValue; // get <comment> tag text value.
+        this.setState({
+        //data: xmlString,
+        data: `result = ${nodeValue}, comment = ${nodeValue2}`
+        })
+      }
+      else {
+        txtErr = `Request onreadystate event - status ${xhr.status}, readyState ${xhr.readyState}`
+        console.log(txtErr)
+        this.setState({ data: txtErr,})
+      }
+    }
+    */
+
     xhr.onerror = () => {
       txtErr = `Request failed -> onerror event occured.`
       console.log(txtErr)
@@ -51,9 +85,16 @@ class App extends Component {
       console.log(txtErr)
       this.setState({ data: txtErr,})
     }
-    
-    xhr.send();
-    
+
+    //xhr.send(); // for GET case.
+
+    //Send the proper header information along with the request
+    //xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("Content-Type", "text/plain");
+    xhr.send("foo=bar&lorem=ipsum");
+    // xhr.send(new Int8Array()); 
+    // xhr.send(document);
+
   } // end of componentDidMount.
   
   render() {
