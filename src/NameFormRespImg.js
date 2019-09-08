dad5f5775a0c38cc61b74a80f5b247a844301b8e
AKaarna <akaarna@unl.test>
2019-09-08 18:59:01 +0300

    RespImg

--- src/NameFormRespImg.js
+++ src/NameFormRespImg.js
@@ -33,10 +33,6 @@ class NameForm extends Component {
     return (
     <div>
 
-      <header>
-      
-      </header>
-  
       <main>
         <section>
           <h1>My website</h1>
@@ -44,20 +40,20 @@ class NameForm extends Component {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget venenatis ligula. Ut lacinia at dolor vitae pulvinar. Aliquam pretium dignissim eros. Integer faucibus, dui non aliquet bibendum, lectus orci lobortis odio, ornare porttitor est tellus eget velit. Nulla eros elit, malesuada id neque vel, viverra vehicula neque. Nullam auctor turpis non leo iaculis finibus. Quisque blandit arcu venenatis libero tempor, ac pulvinar ligula dapibus.</p>
           
           <picture>
-            <source media="(max-width: 799px)" srcset="elva-480w-close-portrait.jpg"></source>
-            <source media="(min-width: 800px)" srcset="elva-800w.jpg"></source>
-            <img src="elva-800w.jpg" alt="Chris standing up holding his daughter Elva"></img>
+            <source media="(max-width: 799px)" srcSet="elva-480w-close-portrait.jpg"></source>
+            <source media="(min-width: 800px)" srcSet="elva-800w.jpg"></source>
+            <img src="elva-800w.jpg" alt="Chris standing up holding his daughter Elva"/>
           </picture>
   
           <p>Suspendisse potenti. Ut in luctus eros. Mauris pulvinar vehicula aliquet. Etiam imperdiet eleifend luctus. Duis ut justo nec eros ornare consectetur. Vestibulum convallis condimentum varius. Maecenas rutrum porta varius. Phasellus volutpat sem id sagittis luctus. Morbi vitae quam vitae nisi iaculis dignissim.</p>
   
-          <img srcset="elva-fairy-320w.jpg 320w,
+          <img srcSet="elva-fairy-320w.jpg 320w,
                        elva-fairy-480w.jpg 480w,
                        elva-fairy-800w.jpg 800w"
                sizes="(max-width: 320px) 280px,
                       (max-width: 480px) 440px,
                       800px"
-               src="elva-fairy-800w.jpg" alt="Elva dressed as a fairy"> </img>
+               src="elva-fairy-800w.jpg" alt="Elva dressed as a fairy"/>
   
           <p>Header image originally by <a href="https://www.flickr.com/photos/miwok/17086751527/">Miwok</a>.</p>
         </section>
