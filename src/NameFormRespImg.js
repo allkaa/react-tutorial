dcc032bbbdcc80a852fbc79cc1603593e8278992
AKaarna <akaarna@unl.test>
2019-09-08 13:14:07 +0300

    RespImg

--- src/NameFormRespImg.js
+++ src/NameFormRespImg.js
@@ -12,20 +12,11 @@ class NameForm extends Component {
       essay:
         '',
       fruit:
-        ['Coconut','Lime'],
-      checkOption:
-        ['option1'],
-      radioOption:
-        'option1'
+        ['Coconut','Lime']
     }; // very initial value.
-    // Create a ref to the DOM node to access file(s) in a submit handler:
-    this.fileInput = React.createRef();
-
     console.log(this.state.name);
     console.log(this.state.essay);
     console.log(this.state.fruit);
-
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   // Code is invoked after the component is mounted/inserted into the DOM tree.
@@ -36,110 +27,42 @@ class NameForm extends Component {
     console.log(this.state.name);
     console.log(this.state.essay);
     console.log(this.state.fruit);
-    console.log(this.state.checkOption);
-    console.log(this.state.radioOption);
   }
 
-  handleSubmit(event) {
-    console.log('========> NameFormFile handleSubmit <==========')
-    console.log('event: ' + event);
-    console.log(event);
-    console.log('event.target: ' + event.target);
-    console.log(event.target);
-    console.log('event.target.name: ' + event.target.name);
-    console.log(event.target.name);
-    console.log('event.target.type: ' + event.target.type)
-    console.log(event.target.type)
-    console.log('event.target.value: ' + event.target.value);
-    console.log(event.target.value);
-    console.log(this.state);
-    //alert('Name: "' + this.state.name + '" and an essay: "' + this.state.essay + '" and fruit: "' + this.state.fruit + '" were submitted.');
-    event.preventDefault(); // prevent standard form submit to take control over.
-    // Use created ref to the DOM node to access file(s) in a submit handler:
-    alert(
-      `Selected file to upload - ${
-        this.fileInput.current.files[0].name
-      }`
-    );
-    // Access the form element...
-    let form = document.getElementById("myForm");
-    // Bind the FormData object and the form element
-    let FD = new FormData(form);
-
-    let txtErr 
-    //let objThis = this
-    const xhr = new XMLHttpRequest();
-
-    //xhr.open('GET', 'http://10.8.194.3:42001/?testDebian', true); // true is open async.
-    //xhr.open('POST', 'http://10.8.194.3:42001/?testDebian', true);
-    xhr.open('POST', 'http://unl.test:8081/fileupload', true);
-    // If specified, responseType must be empty string, "text" or "document"
-    //xhr.responseType = 'document';
-    xhr.responseType = 'text';
-    // Force the response to be parsed as XML
-    //xhr.overrideMimeType('text/xml');
-    //xhr.overrideMimeType('text/html');
-    xhr.overrideMimeType('text/plain');
-
-    // GET or PUT state case using onload event.
-    xhr.onload = () => {
-      console.log('========> FormFile event xhr.onload xhr.AllResponseHeaders<==========')
-      console.log(xhr.getAllResponseHeaders());
-      let docXml
-      if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-        //console.log(xhr.response);
-        /*
-        docXml = xhr.responseXML
-        //console.log(docXml);
-        let xmlS = new XMLSerializer();
-        let xmlString = xmlS.serializeToString(docXml);
-        console.log('========> FormData event xhr.onload subevent xmlSting <==========')
-        console.log(xmlString)
-        let nodeValue = docXml.getElementsByTagName("result")[0].childNodes[0].nodeValue; // get <result> tag text value.
-        let nodeValue2 = docXml.getElementsByTagName("comment")[0].childNodes[0].nodeValue; // get <comment> tag text value.
-        */
-        docXml = xhr.responseText;
-        this.setState({
-          //data: xmlString,
-          essay: `Server reply: ${docXml}\nFile ${this.fileInput.current.files[0].name} uploaded.`
-        })
-        console.log('========> FormFile event xhr.onload xhr.responseText<==========')
-        console.log(docXml);
-      }
-      else {
-        txtErr = `Request onload error - status ${xhr.status}, readyState ${xhr.readyState}`
-        console.log(txtErr)
-        this.setState({ data: txtErr,})
-        }
-    }
-    //
-
-    xhr.onerror = () => {
-      txtErr = `Request failed -> onerror event occured.`
-      console.log(txtErr)
-      this.setState({ data: txtErr,})
-    }
-    
-    xhr.ontimeout = () => {
-      txtErr = `Request failed -> ontimeout event occured`
-      console.log(txtErr)
-      this.setState({ data: txtErr,})
-    }
-
-    console.log('========> FormDataFile submit event to send <==========')
-    console.log(FD);
-    // Send our FormData object; HTTP headers are set automatically!!!
-    //xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); <---- do not use with FD.
-    xhr.send(FD);
-
-  } // end of handleSubmit(event)
-
-  // onSubmit={this.handleSubmit} action="http://unl.test:8081/fileupload" method="post" enctype="multipart/form-data"
-  // formMethod="post" formAction="http://unl.test:8081/fileupload" enctype="multipart/form-data"
   render() {
     return (
     <div>
 
+      <header>
+      
+      </header>
+  
+      <main>
+        <section>
+          <h1>My website</h1>
+  
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget venenatis ligula. Ut lacinia at dolor vitae pulvinar. Aliquam pretium dignissim eros. Integer faucibus, dui non aliquet bibendum, lectus orci lobortis odio, ornare porttitor est tellus eget velit. Nulla eros elit, malesuada id neque vel, viverra vehicula neque. Nullam auctor turpis non leo iaculis finibus. Quisque blandit arcu venenatis libero tempor, ac pulvinar ligula dapibus.</p>
+          
+          <picture>
+            <source media="(max-width: 799px)" srcset="elva-480w-close-portrait.jpg"></source>
+            <source media="(min-width: 800px)" srcset="elva-800w.jpg"></source>
+            <img src="elva-800w.jpg" alt="Chris standing up holding his daughter Elva"></img>
+          </picture>
+  
+          <p>Suspendisse potenti. Ut in luctus eros. Mauris pulvinar vehicula aliquet. Etiam imperdiet eleifend luctus. Duis ut justo nec eros ornare consectetur. Vestibulum convallis condimentum varius. Maecenas rutrum porta varius. Phasellus volutpat sem id sagittis luctus. Morbi vitae quam vitae nisi iaculis dignissim.</p>
+  
+          <img srcset="elva-fairy-320w.jpg 320w,
+                       elva-fairy-480w.jpg 480w,
+                       elva-fairy-800w.jpg 800w"
+               sizes="(max-width: 320px) 280px,
+                      (max-width: 480px) 440px,
+                      800px"
+               src="elva-fairy-800w.jpg" alt="Elva dressed as a fairy"> </img>
+  
+          <p>Header image originally by <a href="https://www.flickr.com/photos/miwok/17086751527/">Miwok</a>.</p>
+        </section>
+      </main>
+  
 
     </div> 
     );
