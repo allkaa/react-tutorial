6a018c72bc1319fa3dd71ca066ca0404a2263110
AKaarna <akaarna@unl.test>
2019-09-14 11:10:56 +0300

    WebStuct

--- src/NameFormWebStuct.js
+++ src/NameFormWebStuct.js
@@ -33,37 +33,48 @@ class NameForm extends Component {
     return (
     <div>
 
-<h1>Birdwatching</h1>
-      <img src="dove.png" alt="a simple dove logo"/>
+      <header>
+        <h1>Birdwatching</h1>
+        <img src="dove.png" alt="a simple dove logo"/>
+      </header>
 
+      <nav>
+        <ul>
+          <li><span>Home</span></li>
+          <li><a href="#1">Get started</a></li>
+          <li><a href="#2">Photos</a></li>
+          <li><a href="#3">Gear</a></li>
+          <li><a href="#4">Forum</a></li>
+        </ul>
+      </nav>
 
-      <ul>
-        <li><span>Home</span></li>
-        <li><a href="#1">Get started</a></li>
-        <li><a href="#2">Photos</a></li>
-        <li><a href="#3">Gear</a></li>
-        <li><a href="#4">Forum</a></li>
-      </ul>
+      <main>
 
+      <article>
+        <h2>Welcome</h2>
+        <section>
+          <p>Welcome to our fake birdwatching site. If this were a real site, it would be the ideal place to come to learn more about birdwatching, whether you are a beginner looking to learn how to get into birding, or an expert wanting to share ideas, tips, and photos with other like-minded people.</p>
+        </section>
+        <section>
+          <p>So don't waste time! Get what you need, then turn off that computer and get out into the great outdoors!</p>
+        </section>
+      </article>
 
-      <h2>Welcome</h2>
+      <aside>
+        <h2>Favourite photos</h2>
+        <a href="favorite-1.jpg"><img src="favorite-1_th.jpg" alt="Small black bird, black claws, long black slender beak"/></a>
+        <a href="favorite-2.jpg"><img src="favorite-2_th.jpg" alt="Top half of a pretty bird with bright blue plumage on neck, light colored beak, blue headdress"/></a>
+        <a href="favorite-3.jpg"><img src="favorite-3_th.jpg" alt="Top half of a large bird with white plumage, very long curved narrow light colored break"/></a>
+        <a href="favorite-4.jpg"><img src="favorite-4_th.jpg" alt="Large bird, mostly white plumage with black plumage on back and rear, long straight white beak"/></a>
+      </aside>
 
-      <p>Welcome to our fake birdwatching site. If this were a real site, it would be the ideal place to come to learn more about birdwatching, whether you are a beginner looking to learn how to get into birding, or an expert wanting to share ideas, tips, and photos with other like-minded people.</p>
+      </main>
 
-      <p>So don't waste time! Get what you need, then turn off that computer and get out into the great outdoors!</p>
+      <footer>
+        <p>This fake website example is CC0 — any part of this code may be reused in any way you wish. Original example written by Chris Mills, 2016.</p>
+        <p><a href="http://game-icons.net/lorc/originals/dove.html">Dove icon</a> by Lorc.</p>
+      </footer>
 
-      <h2>Favourite photos</h2>
-
-      <a href="favorite-1.jpg"><img src="favorite-1_th.jpg" alt="Small black bird, black claws, long black slender beak, links to larger version of the image"/></a>
-      <a href="favorite-2.jpg"><img src="favorite-2_th.jpg" alt="Top half of a pretty bird with bright blue plumage on neck, light colored beak, blue headdress, links to larger version of the image"/></a>
-      <a href="favorite-3.jpg"><img src="favorite-3_th.jpg" alt="Top half of a large bird with white plumage, very long curved narrow light colored break, links to larger version of the image"/></a>
-      <a href="favorite-4.jpg"><img src="favorite-4_th.jpg" alt="Large bird, mostly white plumage with black plumage on back and rear, long straight white beak, links to larger version of the image"/></a>
-
-
-      <p>This fake website example is CC0 — any part of this code may be reused in any way you wish. Original example written by Chris Mills, 2016.</p>
-
-      <p><a href="http://game-icons.net/lorc/originals/dove.html">Dove icon</a> by Lorc.</p>
-  
     </div> 
     );
   }
