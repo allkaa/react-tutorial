48aaac064977d99463d8de6401566de0ce889a77
AKaarna <akaarna@unl.test>
2019-09-17 10:28:31 +0300

    input search

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const iframe = document.getElementById('glm')
-iframe.setAttribute('src', 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d20333.535337350597!2d30.61040335!3d50.42821145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sua!4v1567495244815!5m2!1sen!2sua');
+//const iframe = document.getElementById('glm')
+//iframe.setAttribute('src', 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d20333.535337350597!2d30.61040335!3d50.42821145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sua!4v1567495244815!5m2!1sen!2sua');
 
 
 /*
