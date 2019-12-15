d87590d5a46f1534c59d371f071165d4fc78fd50
AKaarna <akaarna@unl.test>
2019-12-15 13:04:49 +0200

    refs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 //import PropTypes from 'prop-types';
 
-//const myRef = React.createRef(); // create empty ref construction.
 class App extends Component {
    constructor(props) {
       super(props);
@@ -18,12 +17,13 @@ class App extends Component {
    }
    clearInput() {
       this.setState({data: ''});
-      this.myRef.current.focus(); // does NOT work ref to <input .../> is used to set focus, ref set in <input .../>
+      this.myRef.current.focus(); // ref to <input .../> is used to set focus, ref set in <input .../>
       //ReactDOM.findDOMNode(this.refs.myInput).focus(); // ref to <input .../> is used to set focus, ref set in <input .../>
    }
    render() {
       return (
          <div>
+            {/* using ref set this.myRef.current to <input ... /> */}
             <input value = {this.state.data} onChange = {this.updateState} ref = {this.myRef}></input>
             {/*<input value = {this.state.data} onChange = {this.updateState} ref = "myInput"></input>*/}
             <button type="button" onClick = {this.clearInput}>CLEAR</button>
@@ -34,23 +34,3 @@ class App extends Component {
 }
 
 export default App;
-
-// Create a ref in constructor to the DOM node to access file(s) in a submit handler:
-/*
-    this.fileInput = React.createRef();
-*/
-// In render return() set <input> ref
-/*
-        <div>
-          <label htmlFor="file_name">Upload file:: <abbr title="required">*</abbr> </label>
-          <input type="file" id="file_name" ref={this.fileInput} />
-        </div>
-*/
-// In handleSubmit(event) use created ref to the DOM node to access file(s) in a submit handler:
-/*
-    alert(
-      `Selected file to upload - ${
-        this.fileInput.current.files[0].name
-      }`
-    );
-*/
