414e70bcef4533309a4f3125722d9d0fdcc0c072
AKaarna <akaarna@unl.test>
2019-12-15 13:00:00 +0200

    refs

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import ReactDOM from 'react-dom';
 class App extends Component {
    constructor(props) {
       super(props);
-	
       this.state = {
          data: ''
       }
+      this.myRef = React.createRef(); // create empty ref construction.
       this.updateState = this.updateState.bind(this);
       this.clearInput = this.clearInput.bind(this);
    };
@@ -18,14 +18,14 @@ class App extends Component {
    }
    clearInput() {
       this.setState({data: ''});
-      //ReactDOM.findDOMNode(this.refs.myRef).focus(); // does NOT work ref to <input .../> is used to set focus, ref set in <input .../>
-      ReactDOM.findDOMNode(this.refs.myInput).focus(); // ref to <input .../> is used to set focus, ref set in <input .../>
+      this.myRef.current.focus(); // does NOT work ref to <input .../> is used to set focus, ref set in <input .../>
+      //ReactDOM.findDOMNode(this.refs.myInput).focus(); // ref to <input .../> is used to set focus, ref set in <input .../>
    }
    render() {
       return (
          <div>
-            {/*<input value = {this.state.data} onChange = {this.updateState} ref = {myRef}></input>*/}
-            <input value = {this.state.data} onChange = {this.updateState} ref = "myInput"></input>
+            <input value = {this.state.data} onChange = {this.updateState} ref = {this.myRef}></input>
+            {/*<input value = {this.state.data} onChange = {this.updateState} ref = "myInput"></input>*/}
             <button type="button" onClick = {this.clearInput}>CLEAR</button>
             <h4>{this.state.data}</h4>
          </div>
@@ -33,4 +33,24 @@ class App extends Component {
    }
 }
 
-export default App;  
+export default App;
+
+// Create a ref in constructor to the DOM node to access file(s) in a submit handler:
+/*
+    this.fileInput = React.createRef();
+*/
+// In render return() set <input> ref
+/*
+        <div>
+          <label htmlFor="file_name">Upload file:: <abbr title="required">*</abbr> </label>
+          <input type="file" id="file_name" ref={this.fileInput} />
+        </div>
+*/
+// In handleSubmit(event) use created ref to the DOM node to access file(s) in a submit handler:
+/*
+    alert(
+      `Selected file to upload - ${
+        this.fileInput.current.files[0].name
+      }`
+    );
+*/
